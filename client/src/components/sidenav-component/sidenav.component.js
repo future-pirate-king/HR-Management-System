3821f1a080b01bd317f63a92814c5d79f225eb7b
future-pirate-king <samd948@gmail.com>
2018-09-16 19:09:50 +0530

    side nav template

--- client/src/components/sidenav-component/sidenav.component.js
+++ client/src/components/sidenav-component/sidenav.component.js
@@ -4,4 +4,34 @@ app.component('sidenavComponent', {
   controller: sidenavController
 });
 
-function sidenavController() {}
+function sidenavController() {
+  var $ctrl = this;
+
+  $ctrl.menuItems = [
+    {
+      icon: 'fas fa-home fa-lg',
+      title: 'Home',
+      link: 'home'
+    },
+    {
+      icon: 'fas fa-calendar-alt fa-lg',
+      title: 'Time Sheet',
+      link: 'timeSheet'
+    },
+    {
+      icon: 'fas fa-money-bill-alt fa-lg',
+      title: 'Payroll',
+      link: 'payroll'
+    },
+    {
+      icon: 'fas fa-comment-dots fa-lg',
+      title: 'Complaints',
+      link: 'complaints'
+    },
+    {
+      icon: 'fab fa-foursquare fa-lg',
+      title: 'Feedback',
+      link: 'feedback'
+    }
+  ];
+}
