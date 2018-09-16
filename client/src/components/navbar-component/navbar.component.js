c194e6512a6c795e01f6eff77f729ca781d72f7c
future-pirate-king <samd948@gmail.com>
2018-09-16 16:56:07 +0530

    Added sidenav and navbar components

---
+++ client/src/components/navbar-component/navbar.component.js
@@ -0,0 +1,7 @@
+app.component('navbarComponent', {
+  bindings: {},
+  templateUrl: './src/components/navbar-component/navbar.component.html',
+  controller: navbarController
+});
+
+function navbarController() {}
---
+++ client/src/components/sidenav-component/sidenav.component.js
@@ -0,0 +1,7 @@
+app.component('sidenavComponent', {
+  bindings: {},
+  templateUrl: './src/components/sidenav-component/sidenav.component.html',
+  controller: sidenavController
+});
+
+function sidenavController() {}
