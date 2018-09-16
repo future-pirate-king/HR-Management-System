038d5d626063a979d0803739107c9a89eba61b4f
future-pirate-king <samd948@gmail.com>
2018-09-16 17:05:14 +0530

    added main-area component

---
+++ client/src/components/main-area-component/main-area.component.js
@@ -0,0 +1,7 @@
+app.component('mainAreaComponent', {
+  bindings: {},
+  templateUrl: './src/components/main-area-component/main-area.component.html',
+  controller: mainAreaController
+});
+
+function mainAreaController() {}
