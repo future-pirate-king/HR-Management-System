af2d7fecc4ee0a01367d098992c723eb13f47f81
future-pirate-king <samd948@gmail.com>
2018-09-21 00:29:01 +0530

    added employee controller

--- backend-server/src/com/project/hrmanagement/controller/EmployeeController.java
+++ backend-server/src/com/project/hrmanagement/controller/EmployeeController.java
@@ -1,13 +1,25 @@
 package com.project.hrmanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.hrmanagement.model.Employee;
+import com.project.hrmanagement.model.LoginCredential;
+import com.project.hrmanagement.service.IEmployeeService;
+
 @RestController
 public class EmployeeController {
-	
-	@RequestMapping("/hello")
-	public String showHello() {
-		return "Hello";
+
+	@Autowired
+	private IEmployeeService empService;
+
+	@RequestMapping("/authenticate")
+	public Employee authenticate() {
+		//problem with id
+		LoginCredential credential = new LoginCredential(new Integer(10653184), "sam");
+		
+
+		return empService.authenticate(credential);
 	}
 }
